788c1270629d94a39eb75474abf7d5dff3bc724e
Sabin Pandey <34089225+sabin-pandey@users.noreply.github.com>
2023-05-14 21:39:54 +0545

    Initial structuing of stat.

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ type GameStatType = {
   goalsScored: number;
   goalsConceded: number;
   goalDifference: number;
-  points: number;
+  points?: number;
 };
 
 type Stat = Record<string, GameStatType>;
@@ -34,21 +34,83 @@ function App() {
   useEffect(() => {
     function formatStat(res: ResFromAPI) {
       const stat = res.matches.reduce((acc: Stat, curr) => {
-        acc[curr.team1] = acc[curr.team1] ?? {};
+        const { score, team1, team2 } = curr;
+
+        const { ft } = score ?? {};
+
+        const [team1Score = 0, team2Score = 0] = ft ?? [];
+
+        acc[team1] = acc[team1] ?? {};
+
+        acc[team2] = acc[team2] ?? {};
 
         const {
-          gamesDrawn = 0,
-          gamesLost = 0,
-          gamesPlayed = 0,
-          gamesWon = 0,
-          goalDifference = 0,
-          goalsConceded = 0,
-          goalsScored = 0,
-          points = 0,
-        } = acc[curr.team1];
+          gamesDrawn: gamesDrawnTeam1 = 0,
+          gamesLost: gamesLostTeam1 = 0,
+          gamesPlayed: gamesPlayedTeam1 = 0,
+          gamesWon: gamesWonTeam1 = 0,
+          goalDifference: goalDifferenceTeam1 = 0,
+          goalsConceded: goalsConcededTeam1 = 0,
+          goalsScored: goalsScoredTeam1 = 0,
+          points: pointsTeam1 = 0,
+        } = acc[team1];
 
-        acc[curr.team1] = acc[curr.team1].gamesPlayed ?? 0 + 1;
-      }, []);
+        const {
+          gamesDrawn: gamesDrawnTeam2 = 0,
+          gamesLost: gamesLostTeam2 = 0,
+          gamesPlayed: gamesPlayedTeam2 = 0,
+          gamesWon: gamesWonTeam2 = 0,
+          goalDifference: goalDifferenceTeam2 = 0,
+          goalsConceded: goalsConcededTeam2 = 0,
+          goalsScored: goalsScoredTeam2 = 0,
+          points: pointsTeam2 = 0,
+        } = acc[team2];
+
+        if (score !== undefined) {
+          acc[team1].gamesPlayed = gamesPlayedTeam1 + 1;
+          acc[team2].gamesPlayed = gamesPlayedTeam2 + 1;
+        }
+
+        if (ft !== undefined) {
+          const goalDifference = team1Score - team2Score;
+
+          if (goalDifference === 0) {
+            acc[team1].gamesDrawn = gamesDrawnTeam1 + 1;
+
+            acc[team2].gamesDrawn = gamesDrawnTeam2 + 1;
+
+            acc[team1].points = pointsTeam1 + 1;
+
+            acc[team2].points = pointsTeam2 + 1;
+          } else if (goalDifference > 0) {
+            acc[team1].gamesWon = gamesWonTeam1 + 1;
+
+            acc[team2].gamesLost = gamesLostTeam2 + 1;
+
+            acc[team1].points = (acc[team1].points ?? 0) + 3;
+
+            acc[team1].goalDifference = goalDifferenceTeam1 + goalDifference;
+
+            acc[team2].goalsConceded = goalsConcededTeam2 + team1Score;
+
+            acc[team1].goalsScored = goalsScoredTeam1 + team1Score;
+          } else {
+            acc[team1].gamesLost = gamesLostTeam1 + 1;
+
+            acc[team2].gamesWon = gamesWonTeam2 + 1;
+
+            acc[team2].points = (acc[team2].points ?? 0) + 3;
+
+            acc[team1].goalDifference = goalDifferenceTeam2 + goalDifference;
+
+            acc[team1].goalsConceded = goalsConcededTeam1 + team2Score;
+
+            acc[team2].goalsScored = goalsScoredTeam2 + team2Score;
+          }
+        }
+
+        return acc;
+      }, {});
 
       console.log(stat);
     }
@@ -56,11 +118,15 @@ function App() {
     const url =
       "https://raw.githubusercontent.com/openfootball/football.json/master/2020-21/en.1.json";
 
-    fetch(url).then(async (res) => {
-      const actualRes = (await res.json()) as ResFromAPI;
+    fetch(url)
+      .then(async (res) => {
+        const actualRes = (await res.json()) as ResFromAPI;
 
-      formatStat(actualRes);
-    });
+        formatStat(actualRes);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
   }, []);
 
   return <></>;
